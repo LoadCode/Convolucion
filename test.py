31cdc8821c8fa970bb188c7216790ba827cf1c19
Julio C. Echeverri M <julio.em7@gmail.com>
2015-11-16 10:43:19 -0500

    archivo de pruebas

--- test.py
+++ test.py
@@ -1,19 +1,20 @@
-import convolucion as conv
+from convolucion import *
 
-a = [1,0.5,3]
-b = [1,0.5,-3]
+##z = 1+5*1j
+##poli = convolucion([1,-z],[1,-z.conjugate()])
+##poli = convolucion(poli,[1,-4])
+##
+##for k in range(0,len(poli)):
+##    poli[k] = poli[k].real
+##
+##print poli 
 
-y = conv.convolucion(b,a)
-#print y
 
 
 
-def poly(polo_1, polo_2):
-    poli = conv.convolucion([1, polo_1[0], polo_1[1]], [1, polo_2[0], polo_2[1]])
-    poli[2] = poli[2]-poli[4]
-    poli = poli[0:3]
-    poli[1] *= -1
-    return poli
+
+
+
 
 
 #Obtener los coeficientes de un polinomio a partir de sus raices
@@ -22,10 +23,10 @@ def poly(polo_1, polo_2):
 # s^2+3s-1      polos = 0.302775638   -3.302775638
 # s^2-2s+1      polos = 1
 # s^2-9s+0.5    polos = -0.5+-J3
-sig = -0.5;            w = 3;
-Polo_1 = [0.302775638, 0]
-Polo_2 = [-3.302775638, 0]
-
-Polinom = poly(Polo_1, Polo_2)
-
-print Polinom
+##sig = -0.5;            w = 3;
+##Polo_1 = [0.302775638, 0]
+##Polo_2 = [-3.302775638, 0]
+##
+##Polinom = poly(Polo_1, Polo_2)
+##
+##print Polinom
